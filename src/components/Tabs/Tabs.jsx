3074621b6ff8386b99ef9442675cd7c500c728e0
Dikanb <dikanb1502@gmail.com>
2025-05-21 23:50:58 +0300

    add task solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -2,35 +2,37 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
   const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
   return (
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
-        <ul>
-          {tabs.map(tab => (
-            <li
-              key={tab.id}
-              className={tab.id === activeTab.id ? 'is-active' : ''}
-              data-cy="Tab"
-            >
-              <a
-                href={`#${tab.id}`}
-                data-cy="TabLink"
-                onClick={event => {
-                  event.preventDefault();
-                  if (tab.id !== activeTab.id) {
-                    onTabSelected(tab.id);
-                  }
-                }}
+    <>
+      <h1 className="title">Selected tab is {activeTab.title}</h1>
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tab => (
+              <li
+                key={tab.id}
+                className={tab.id === activeTab.id ? 'is-active' : ''}
+                data-cy="Tab"
               >
-                {tab.title}
-              </a>
-            </li>
-          ))}
-        </ul>
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={() => {
+                    if (activeTabId !== tab.id) {
+                      onTabSelected(tab.id);
+                    }
+                  }}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
 
       <div className="block" data-cy="TabContent">
         {activeTab.content}
       </div>
-    </div>
+    </>
   );
 };
